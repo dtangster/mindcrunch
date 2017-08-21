76799b5eb304e2c27e53f3ffa92b322a8e6fd7c5
David Tang <david.tang@yandex.com>
2017-08-21 05:22:38 +0000

    Rename backend to phoenix

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-docker run \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:/mindcrunch" \
-    -w "/mindcrunch" \
-    docker/compose:1.15.0 build --force-rm
-
-docker run \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:/mindcrunch" \
-    -w "/mindcrunch" \
-    docker/compose:1.15.0 up
+docker-compose build --force-rm
+docker-compose up
