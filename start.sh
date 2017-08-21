ed44f037c186682b775c9c732b73dcc5fe44eb3e
David Tang <david.tang@yandex.com>
2017-08-21 03:36:06 +0000

    Run docker-compose from a container

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker-compose build --force-rm
-docker-compose up
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:/mindcrunch" \
+    -w "/mindcrunch" \
+    docker/compose:1.15.0 build --force-rm
+
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:/mindcrunch" \
+    -w "/mindcrunch" \
+    docker/compose:1.15.0 up
