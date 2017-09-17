58596d2cd7bdb69c52b12f34fddaedba77f395d9
David Tang <david.tang@yandex.com>
2017-09-17 22:51:04 +0000

    Separate index.html for webpack-dev-server and phoenix

--- mindcrunch/ui/webpack.config.js
+++ mindcrunch/ui/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
       { from: 'src/templates/app.html.eex', to: 'templates/app.html.eex' },
+      { from: 'src/index.html', to: 'index.html' }
     ])
   ]
 }
